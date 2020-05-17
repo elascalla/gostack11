1c19aceea8006e8dea68f0334de2326666a7dbf5
elascalla <erick.lascalla@gmail.com>
2020-05-17 12:41:18 -0400

    Initial back-end Gobarber

---
+++ gobarber-back-end/src/services/CreateAppointmentService.ts
@@ -0,0 +1,37 @@
+import { startOfHour } from 'date-fns';
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+interface Request {
+  provider: string;
+  date: Date;
+}
+
+class CreateAppointmentService {
+  private appointmentsRepository: AppointmentsRepository;
+
+  constructor(appointmentsRepository: AppointmentsRepository) {
+    this.appointmentsRepository = appointmentsRepository;
+  }
+
+  public execute({ provider, date }: Request): Appointment {
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw Error('Appointments in the same date.');
+    }
+
+    const appointment = this.appointmentsRepository.create({
+      provider,
+      date: appointmentDate,
+    });
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
