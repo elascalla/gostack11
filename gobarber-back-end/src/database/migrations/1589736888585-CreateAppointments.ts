55e3be79903056fbdbb554afef3afd9cb7d60f20
elascalla <erick.lascalla@gmail.com>
2020-05-25 19:49:25 -0400

    Feat: Create Appointments and users and authentic

---
+++ gobarber-back-end/src/database/migrations/1589736888585-CreateAppointments.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateAppointments1589736888585 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+    }
+
+}
---
+++ gobarber-back-end/src/database/migrations/1589752530935-AlterProviderFieldProviderId.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class AlterProviderFieldProviderId1589752530935 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+    }
+
+}
