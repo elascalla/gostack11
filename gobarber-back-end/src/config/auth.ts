a8dd8d3e561173bf5821614cbb2a34eb88bfc5b2
elascalla <erick.lascalla@gmail.com>
2020-05-25 19:49:44 -0400

    Feat: Create Appointments and users and authentic

---
+++ gobarber-back-end/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '24714d4faab94dc74d7d923eedd88971',
+    expiresIn: '1d',
+  },
+};
--- gobarber-back-end/src/database/migrations/1589752530935-AlterProviderFieldProviderId.ts
+++ gobarber-back-end/src/database/migrations/1589752530935-AlterProviderFieldProviderId.ts
@@ -1,11 +1,47 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
 
-export class AlterProviderFieldProviderId1589752530935 implements MigrationInterface {
+export default class AlterProviderFieldProviderId1589752530935
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn('appointments', 'provider');
+    await queryRunner.addColumn(
+      'appointments',
+      new TableColumn({
+        name: 'provider_id',
+        type: 'varchar',
+        isNullable: true,
+      }),
+    );
 
-    public async up(queryRunner: QueryRunner): Promise<any> {
-    }
+    await queryRunner.createForeignKey(
+      'appointments',
+      new TableForeignKey({
+        name: 'AppointmentProvider',
+        columnNames: ['provider_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<any> {
-    }
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('appointments', 'AppointmentProvider');
 
+    await queryRunner.dropColumn('appointments', 'provider_id');
+
+    await queryRunner.addColumn(
+      'appointments',
+      new TableColumn({
+        name: 'provider',
+        type: 'varchar',
+      }),
+    );
+  }
 }
