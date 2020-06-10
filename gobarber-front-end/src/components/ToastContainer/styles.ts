1259e33fda7ae7158067c442aeca7dab583bf198
elascalla <erick.lascalla@gmail.com>
2020-06-10 15:17:39 -0400

    Feat: Commit Front-end GoBarber

---
+++ gobarber-front-end/src/components/ToastContainer/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  position: absolute;
+  right: 0;
+  top: 0;
+  padding: 30px;
+  overflow: hidden; // Tudo que estiver mostrando além do tamanho do container irá ficar invisível
+`;
